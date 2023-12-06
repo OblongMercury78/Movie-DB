b9c6e20c296ea323f2fa66d1e8dc717d98738c43
Mohammed Hisham <m.hishamicloud@gmail.com>
2023-12-06 22:41:06 +0530

    final edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,262 @@
-import React, {useState, useEffect} from 'react' 
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const MembersPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    release_year: '',
+    genre: '',
+    link: '',
+    staff_name: [],
+    role: [],
+    studio: [],
+    reviewer_id: [],
+    rating: [],
+    age_rating: [],
+  });
 
-  const [data, setData] = useState([{}])
+  const [movies, setMovies] = useState([]);
+  const [changes, setChanges] = useState({
+    movies: [],
+    productions: [],
+    reviews: [],
+  });
 
   useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
+    // Fetch the movie list when the component mounts
+    fetchMovies();
+  }, []); // Empty dependency array ensures this effect runs once on mount
+
+  const fetchMovies = async () => {
+    try {
+      // Assuming you have an API endpoint to fetch the movie list
+      const response = await fetch('/api/movies');
+      if (response.ok) {
+        const data = await response.json();
+        setMovies(data.movies);
+      } else {
+        console.error('Error:', response.status);
       }
-    )
-  }, [])
-  return(
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleArrayInputChange = (e, field, index) => {
+    const { value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [field]: prevData[field].map((item, i) => (i === index ? value : item)),
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Assuming you have an API endpoint to handle the form data
+      const response = await fetch('/api/members', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setMovies(data.movies);
+        setChanges(data.changes);
+        setFormData({
+          name: '',
+          release_year: '',
+          genre: '',
+          link: '',
+          staff_name: [],
+          role: [],
+          studio: [],
+          reviewer_id: [],
+          rating: [],
+          age_rating: [],
+        });
+      } else {
+        // Handle error cases
+        console.error('Error:', response.status);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const resetForm = () => {
+    setFormData({
+      name: '',
+      release_year: '',
+      genre: '',
+      link: '',
+      staff_name: [],
+      role: [],
+      studio: [],
+      reviewer_id: [],
+      rating: [],
+      age_rating: [],
+    });
+  };
+
+  const resetChanges = () => {
+    window.location.reload();
+  };
+
+  return (
     <div>
+      <h1>Members Page</h1>
 
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-          data.members.map((member,i) => (
-            <p key={i}>{member}</p>
-          ))
-      )}
+      <h2>Add Movie, Production, and Review</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Movie Name:
+          <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
+        </label>
+        <br />
+
+        <label>
+          Release Year:
+          <input type="number" name="release_year" value={formData.release_year} onChange={handleInputChange} required />
+        </label>
+        <br />
+
+        <label>
+          Genre:
+          <input type="text" name="genre" value={formData.genre} onChange={handleInputChange} required />
+        </label>
+        <br />
+
+        <label>
+          Link:
+          <input type="text" name="link" value={formData.link} onChange={handleInputChange} required />
+        </label>
+        <br />
+
+        {/* Production Information */}
+        <h3>Production Information</h3>
+        {formData.staff_name.map((staff, index) => (
+          <div key={index}>
+            <label>
+              Staff Name:
+              <input
+                type="text"
+                value={staff}
+                onChange={(e) => handleArrayInputChange(e, 'staff_name', index)}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Role:
+              <input
+                type="text"
+                value={formData.role[index]}
+                onChange={(e) => handleArrayInputChange(e, 'role', index)}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Studio:
+              <input
+                type="text"
+                value={formData.studio[index]}
+                onChange={(e) => handleArrayInputChange(e, 'studio', index)}
+                required
+              />
+            </label>
+            <br />
+          </div>
+        ))}
+        <button type="button" onClick={() => setFormData((prevData) => ({ ...prevData, staff_name: [...prevData.staff_name, ''], role: [...prevData.role, ''], studio: [...prevData.studio, ''] }))}>
+          Add Another Production
+        </button>
+        <br />
+
+        {/* Review Information */}
+        <h3>Review Information</h3>
+        {formData.reviewer_id.map((reviewer, index) => (
+          <div key={index}>
+            <label>
+              Reviewer ID:
+              <input
+                type="text"
+                value={reviewer}
+                onChange={(e) => handleArrayInputChange(e, 'reviewer_id', index)}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Rating:
+              <input
+                type="number"
+                value={formData.rating[index]}
+                onChange={(e) => handleArrayInputChange(e, 'rating', index)}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Age Rating:
+              <input
+                type="text"
+                value={formData.age_rating[index]}
+                onChange={(e) => handleArrayInputChange(e, 'age_rating', index)}
+                required
+              />
+            </label>
+            <br />
+          </div>
+        ))}
+        <button type="button" onClick={() => setFormData((prevData) => ({ ...prevData, reviewer_id: [...prevData.reviewer_id, ''], rating: [...prevData.rating, ''], age_rating: [...prevData.age_rating, ''] }))}>
+          Add Another Review
+        </button>
+        <br />
+
+        <button type="submit">Add Movie, Production, and Review</button>
+        <button type="button" onClick={resetForm}>
+          Reset Form
+        </button>
+        <button type="button" onClick={resetChanges}>
+          Reset Changes
+        </button>
+      </form>
+
+      <h2>Movie List</h2>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            {/* Display movie information */}
+            {movie.name} ({movie.release_year}) - {movie.genre} - {movie.link}
+            <ul>
+              {movie.productions.map((production) => (
+                <li key={production.id}>
+                  {production.staff_name} - {production.role} - {production.studio}
+                </li>
+              ))}
+            </ul>
+            <ul>
+              {movie.reviews.map((review) => (
+                <li key={review.id}>
+                  Reviewer ID: {review.reviewer_id} - Rating: {review.rating} - Age Rating: {review.age_rating}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default MembersPage;
--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,81 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/members")
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    release_year = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(20), nullable=False)
+    link = db.Column(db.String(40), unique=True, nullable=False)
+    productions = db.relationship('Production', backref='movie', lazy=True)
+    reviews = db.relationship('Review', backref='movie', lazy=True)
+
+class Production(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    staff_name = db.Column(db.String(30), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    studio = db.Column(db.String(40), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reviewer_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    age_rating = db.Column(db.String(30), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+
+@app.route('/members', methods=['GET', 'POST'])
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    if request.method == 'POST':
+        name = request.form['name']
+        release_year = request.form['release_year']
+        genre = request.form['genre']
+        link = request.form['link']
+
+        # Create a new Movies instance and add it to the database
+        new_movie = Movies(name=name, release_year=release_year, genre=genre, link=link)
+        db.session.add(new_movie)
+        db.session.commit()
+
+        # Get the ID of the newly added movie
+        movie_id = new_movie.id
+
+        # Get production information from the form
+        staff_names = request.form.getlist('staff_name[]')
+        roles = request.form.getlist('role[]')
+        studios = request.form.getlist('studio[]')
+
+        # Create Production instances and add them to the database
+        for staff_name, role, studio in zip(staff_names, roles, studios):
+            new_production = Production(staff_name=staff_name, role=role, studio=studio, movie_id=movie_id)
+            db.session.add(new_production)
+
+        # Get review information from the form
+        reviewer_ids = request.form.getlist('reviewer_id[]')
+        ratings = request.form.getlist('rating[]')
+        age_ratings = request.form.getlist('age_rating[]')
+
+        # Create Review instances and add them to the database
+        for reviewer_id, rating, age_rating in zip(reviewer_ids, ratings, age_ratings):
+            new_review = Review(reviewer_id=reviewer_id, rating=rating, age_rating=age_rating, movie_id=movie_id)
+            db.session.add(new_review)
+
+        db.session.commit()
+
+    # Fetch all movies, productions, and reviews from the database
+    all_movies = Movies.query.all()
+
+    # For each movie, fetch associated production and review records
+    for movie in all_movies:
+        movie.productions = Production.query.filter_by(movie_id=movie.id).all()
+        movie.reviews = Review.query.filter_by(movie_id=movie.id).all()
+
+    return render_template('members.html', movies=all_movies)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
